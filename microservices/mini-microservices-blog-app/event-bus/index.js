a56402e06b289cfb864fcca2101c8711cb5433f7
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-10-16 15:52:08 +0600

    add: simple event bus

---
+++ microservices/mini-microservices-blog-app/event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+
+  res.send(event);
+});
+
+app.listen(4005, () => console.log("listening on 4005 (event bus)"));
