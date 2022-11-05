07ff60a12f6d8c99eddcf9fa4c64771fbb40fc34
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-11-05 18:00:25 +0600

    added ingress nginx laod balancer

--- microservices/mini-microservices-blog-app/client/src/CreatePost.js
+++ microservices/mini-microservices-blog-app/client/src/CreatePost.js
@@ -5,7 +5,7 @@ export default () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:4000/posts", { title });
+    await axios.post("http://posts.com/posts/create", { title });
 
     setTitle("");
   };
--- microservices/mini-microservices-blog-app/event-bus/index.js
+++ microservices/mini-microservices-blog-app/event-bus/index.js
@@ -13,9 +13,9 @@ app.post("/events", async (req, res) => {
   events.push(event);
   try {
     await axios.post("http://posts-clusterip-srv:4000/events", event);
-    // await axios.post("http://localhost:4001/events", event);
-    // await axios.post("http://localhost:4002/events", event);
-    // await axios.post("http://localhost:4003/events", event);
+    await axios.post("http://comments-srv:4001/events", event);
+    await axios.post("http://query-srv:4002/events", event);
+    await axios.post("http://moderation-srv:4003/events", event);
   } catch (error) {
     console.log(error);
   }
