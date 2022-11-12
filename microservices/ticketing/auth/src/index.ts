21020f15b327aa1b6b8ff3f67b773951b987a7e7
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-11-12 17:52:26 +0600

    added ingress

--- microservices/ticketing/auth/src/index.ts
+++ microservices/ticketing/auth/src/index.ts
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(4000, () => console.log("Listening on port 4000 (auth)"));
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there");
+});
+
+app.listen(4000, () => console.log("Listening on port 4000 (auth)!"));
